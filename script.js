3803f5969a6a9c394579b550762cff1bf98c2aca
Snigdhendu-Chhotaray-28 <snigdhenduchhotaray@gmail.com>
2025-08-15 17:18:55 +0530

    Artcle page position fn

--- script.js
+++ script.js
@@ -50,3 +50,76 @@ function eye_btn_blink(){
 
 }
 eye_btn_blink();
+
+
+
+
+
+// function fn() {
+//     const divs = document.querySelectorAll('.products_page_div1_div');
+
+//     divs.forEach((div, index) => {
+//         div.addEventListener('mouseenter', () => {
+//             alert(`Mouse is hovering over div number: ${index + 1}`);
+//         });
+
+//         div.addEventListener('mouseleave', () => {
+//             console.log(`Mouse left div number: ${index + 1}`);
+//         });
+//     });
+// }
+// window.onload = fn;
+
+
+
+
+
+
+
+
+// ================== Artcle page movement fn ================== //
+
+let motion_count = 0;
+function artcle_page_position(){
+    let artcle_page_navigation_left_var = document.querySelector('.artcle_page_navigation_left');
+    let artcle_page_navigation_right_var = document.querySelector('.artcle_page_navigation_right');
+
+    let artcle_page_var = document.querySelectorAll('.artcle_page_section');
+    
+    artcle_page_navigation_left_var.addEventListener('click', () => {
+
+        artcle_page_var[motion_count].classList.add('artcle_move_right');
+        artcle_page_var[motion_count].classList.remove('artcle_move_right_reverce');
+
+        if(motion_count > 0){
+            motion_count--;
+        }
+        else{
+            motion_count = 2;
+        }
+
+        artcle_page_var[motion_count].classList.remove('artcle_move_right');
+        artcle_page_var[motion_count].classList.add('artcle_move_right_reverce');
+        
+
+    });
+
+    artcle_page_navigation_right_var.addEventListener('click', () => {
+
+        artcle_page_var[motion_count].classList.add('artcle_move_right');
+        artcle_page_var[motion_count].classList.remove('artcle_move_right_reverce');
+
+        if(motion_count < 2){
+            motion_count++;
+        }
+        else{
+            motion_count = 0;
+        }
+        
+        artcle_page_var[motion_count].classList.remove('artcle_move_right');
+        artcle_page_var[motion_count].classList.add('artcle_move_right_reverce');
+        
+
+    });
+}
+artcle_page_position();
